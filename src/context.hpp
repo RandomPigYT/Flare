3544bdead9e9f0b39c4f866c3cfd823a717d6a51
RandomPigYT <vinecompilation864@gmail.com>
2024-02-12 22:20:25 +0530

    Moved "struct context" to a new file

---
+++ src/context.hpp
@@ -0,0 +1,23 @@
+#ifndef REFLECTION_CONTEXT_H
+#define REFLECTION_CONTEXT_H
+
+#include "typeinfo.hpp"
+#include <clang/AST/ASTConsumer.h>
+
+namespace Reflection {
+
+struct context {
+	using typeInfoVec = std::vector<struct Reflection::typeInfo>;
+	using enumInfoVec = std::vector<struct Reflection::enumInfo>;
+
+	typeInfoVec typeinfo;
+	enumInfoVec enumInfo;
+	std::vector<const char *> args;
+	char *filename;
+
+	clang::ASTContext *context;
+};
+
+}
+
+#endif
