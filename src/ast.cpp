e0ece8dc180ed66e8ba4ee2a39947f848e32f986
Abhay V <abhay.v2024@gmail.com>
2025-02-22 19:57:37 +0530

    Ignore declarations in system headers

--- src/ast.cpp
+++ src/ast.cpp
@@ -26,39 +26,43 @@ private:
 }
 
 bool Reflection::ASTDeclVisitor::TraverseDecl(clang::Decl *D) {
-  if (clang::dyn_cast<clang::TypedefDecl>(D)) {
-    //printf("%s\n", m_ctx.filename);
-  }
+  clang::FullSourceLoc fullSourceLoc(D->getLocation(),
+                                     m_ctx.context->getSourceManager());
+  if (fullSourceLoc.isValid() && !fullSourceLoc.isInSystemHeader()) {
+    if (clang::dyn_cast<clang::TypedefDecl>(D)) {
+      //printf("%s\n", m_ctx.filename);
+    }
 
-  if (clang::RecordDecl *rd = clang::dyn_cast<clang::RecordDecl>(D)) {
-    Reflection::handleRecordDecl(rd, m_ctx);
-  }
+    if (clang::RecordDecl *rd = clang::dyn_cast<clang::RecordDecl>(D)) {
+      Reflection::handleRecordDecl(rd, m_ctx);
+    }
 
-  if (clang::EnumDecl *ed = clang::dyn_cast<clang::EnumDecl>(D)) {
-    Reflection::handleEnumDecl(ed, m_ctx);
-  }
+    if (clang::EnumDecl *ed = clang::dyn_cast<clang::EnumDecl>(D)) {
+      Reflection::handleEnumDecl(ed, m_ctx);
+    }
 
-  if (clang::EnumConstantDecl *ecd =
-        clang::dyn_cast<clang::EnumConstantDecl>(D)) {
-    Reflection::handleEnumConstantDecl(ecd, m_ctx);
-  }
+    if (clang::EnumConstantDecl *ecd =
+          clang::dyn_cast<clang::EnumConstantDecl>(D)) {
+      Reflection::handleEnumConstantDecl(ecd, m_ctx);
+    }
 
-  if (clang::TypedefDecl *td = clang::dyn_cast<clang::TypedefDecl>(D)) {
-    Reflection::handleTypedefDecl(td, m_ctx);
-  }
+    if (clang::TypedefDecl *td = clang::dyn_cast<clang::TypedefDecl>(D)) {
+      Reflection::handleTypedefDecl(td, m_ctx);
+    }
 
-  if (clang::FieldDecl *fd = clang::dyn_cast<clang::FieldDecl>(D)) {
-    Reflection::handleFieldDecl(fd, m_ctx);
-  }
+    if (clang::FieldDecl *fd = clang::dyn_cast<clang::FieldDecl>(D)) {
+      Reflection::handleFieldDecl(fd, m_ctx);
+    }
 
-  if (clang::IndirectFieldDecl *ifd =
-        clang::dyn_cast<clang::IndirectFieldDecl>(D)) {
-    clang::RecordDecl *p =
-      clang::dyn_cast<clang::RecordDecl>(D->getDeclContext());
+    if (clang::IndirectFieldDecl *ifd =
+          clang::dyn_cast<clang::IndirectFieldDecl>(D)) {
+      clang::RecordDecl *p =
+        clang::dyn_cast<clang::RecordDecl>(D->getDeclContext());
 
-    if (!p->isAnonymousStructOrUnion()) {
-      handleFieldDecl(ifd->getAnonField(), m_ctx, p,
-                      m_ctx.context->getFieldOffset(ifd));
+      if (!p->isAnonymousStructOrUnion()) {
+        handleFieldDecl(ifd->getAnonField(), m_ctx, p,
+                        m_ctx.context->getFieldOffset(ifd));
+      }
     }
   }
 
